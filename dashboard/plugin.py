7eaeb75123a8706be3464ed13c29b871c96967e0
Grischabock <info@grischabock.ch>
2026-01-03 15:29:14 +0100

    0.0.3

--- dashboard/plugin.py
+++ dashboard/plugin.py
@@ -16,7 +16,7 @@ class DashboardPlugin(SettingsMixin, UrlsMixin, InvenTreePlugin):
     SLUG = "dashboard"
     TITLE = "Dashboard"
     DESCRIPTION = "Dashboard Plugin für InvenTree mit benutzerdefinierten Links"
-    VERSION = "0.0.2"
+    VERSION = "0.0.3"
     AUTHOR = "GrischaMedia"
     PUBLISH_DATE = "2025-01-01"
     LICENSE = "GPL-3.0"
